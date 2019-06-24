738e42b192d0b49f632965b109de8fc5fbdd8323
tm4c123ghpm6 <meriemdjouaher@outlook.com>
2019-06-24 11:20:15 -0400

    test2

---
+++ Exercice_9.py
@@ -0,0 +1,30 @@
+"""
+Exercise 1
+Write a code that asks the operator for its name and throws an exception if the string is longer than 20 characters
+
+"""
+
+
+def exercise1():
+    print("Hello, this shit is not working! and sitll not working")
+    pass
+
+
+exercise1()
+
+"""
+Exercise 2
+Write a code that asks the operator for its age and tries to convert it to int
+Use the int(string) function: https://docs.python.org/3/library/functions.html#int
+This function will throw an ValueError if it cannot be converted, use the try-catch-else construct to inform 
+the user if the age given was correct or not
+
+"""
+
+
+def exercise2():
+    print("Hello, this shit is not working! and sitll not working")
+    print("Hello,mya be now it is ?")
+    pass
+
+exercise2()
